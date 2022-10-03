38b64216361d6eb8bece514ed674a1865029141b
danmugh <danmmwx@gmail.com>
2022-10-03 23:46:29 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::net::TcpListener;
+use rust_blog::start_blog;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    let listener = TcpListener::bind("0.0.0.0:8080")?;
+	start_blog(listener)?.await?;
+    Ok(())
+}
